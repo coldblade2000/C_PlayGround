4b98430951d1436d467f3e3b29b9446031728fce
coldblade2000 <diegogranada7@gmail.com>
2020-10-29 00:23:30 -0500

    Por fin logre sacar el example string bien! Falta revisar con otras cadenas.
    Ademas implemente el uso de apuntadores en el procedimiento()

--- PlayAround/Proyecto3.c
+++ PlayAround/Proyecto3.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 //Visual Studio se queja si no uso escribo esta bandera
 #define _CRT_SECURE_NO_WARNINGS
 
-//Defino prototipo
-short procedimiento(char caracter, short bitsParidad);
 
 struct Estructura {
-	short bitsParidad;
+	unsigned short bitsParidad;
 	char cadena[17];
 };
+
+//Defino prototipo
+void procedimiento(char caracter, struct Estructura * est);
+
+
 int main()
 {
 	struct Estructura datos;
@@ -18,30 +22,44 @@ int main()
 	printf("Por favor ingresa la cadena de numeros\n");
 	scanf("%s", input);
 	datos.bitsParidad = 0;
-	printf("This is the output: %s", input);
+	printf("This is the output: %s\n", input);
 	memcpy(&datos.cadena, &input, 17);
-	for (char i = 0; i < 17; i++)
+
+	//NOTA: El ejemplo del enunciado tiene un CarriageReturn al final de "Hola", por lo cual asumo que es necesario. SI NO LO ES, BORRAR EL PROXIMO FOR-LOOOP
+	//Este for loop busca el caracter nulo, lo remplaza con un Carriage Return y hace que el proximo caracter sea el caracter nulo.
+	for (short i = 0; i < 17; i++)
 	{
-		datos.bitsParidad = procedimiento(datos.cadena[i], datos.bitsParidad);
+		if (datos.cadena[i] == 0)
+		{
+			datos.cadena[i] = 0x0D;
+			if (i < 16) datos.cadena[i + 1] = 0;
+			break;
+		}
+	}
+
+	//Ejecuta el procedimiento para cada caracter, ignora caracteres que son invalidos
+	for (char i = 16; i >= 0; i--)
+	{
+		if (datos.cadena[i] >= 0) {
+			procedimiento(datos.cadena[i], &datos);
+		}
+		else {
+			procedimiento(0, &datos);
+			
+		}
 		printf("%hu \n", datos.bitsParidad);
 	}
 
-
-	for (char i = 0; i < 17; i++)
+	//Imprime el numero de resultado (que ya esta codificado bien en binario). Imprime bit por bit de mas a menos significante
+	for (char i = 16; i > 0; i--)
 	{
-		printf("%hu", datos.bitsParidad & 1);
-		datos.bitsParidad  = datos.bitsParidad >> 1;
+		unsigned short temp = datos.bitsParidad & (int)pow(2,(i-1));
+		printf("%hu", temp==0? 0: 1);
+		
 	}
-	/*printf("%hu\n", procedimiento(105, 2)); //0
-	printf("%hu\n", procedimiento(106, 2)); //0
-	printf("%hu\n", procedimiento(107, 2)); //1
-	printf("%hu\n", procedimiento(108, 2)); //0
-	printf("%hu\n", procedimiento(109, 2)); //1
-	printf("%hu\n", procedimiento(110, 2)); //1
-	printf("%hu\n", procedimiento(111, 2)); //0*/
 	return 0;
 };
-short procedimiento(char caracter, short bitsParidad) {
+void procedimiento(unsigned char caracter, struct Estructura * est) {
 	short count = 0;
 	for (short ind = 0; ind < 8; ind++){
 		__asm {
@@ -67,7 +85,7 @@ short procedimiento(char caracter, short bitsParidad) {
 		}*/
 	}
 	count = count & 1;//gets the amount of zeroes and checks if its even or odd
-
+	short bitsParidad = est->bitsParidad;
 	//Aca solo sumo count a bitsDePartidad y roto a la derecha. Era mas facil con assembly, ya que con C me toca hacer dos shifts y despues una union... 
 	__asm {
 		mov ax,bitsParidad
@@ -75,6 +93,6 @@ short procedimiento(char caracter, short bitsParidad) {
 		ror ax, 1
 		mov bitsParidad, ax
 	}
-	return bitsParidad;
+	est->bitsParidad = bitsParidad;
 
 }
