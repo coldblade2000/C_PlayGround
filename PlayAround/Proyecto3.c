0e08c310e106e6cdc01fe097afcd89fb4b7dec02
coldblade2000 <diegogranada7@gmail.com>
2020-10-31 11:56:22 -0500

    La transicion a usar VS 2015 x86 native tools fue grave, tenia un error raro. Al fin lo arregle poniendo el tipo correcto en los apuntadores (de int * a short *). Estos cambios fueron dedicados a arreglar ese problema

--- PlayAround/Proyecto3.c
+++ PlayAround/Proyecto3.c
@@ -4,10 +4,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 //Por Diego Granada 201922383
 
-struct Estructura {
-	unsigned short bitsParidad;
-	char cadena[17];
-};
+
 
 //Defino prototipo
 void procedimiento(char caracter, struct Estructura * est);
@@ -15,53 +12,58 @@ void procedimiento(char caracter, struct Estructura * est);
 
 int main()
 {
-	struct Estructura datos;
+
+	struct Estructura {
+		char cadena[17];
+		unsigned short bitsParidad;
+	}datos;
+
 	datos.bitsParidad = 0;
 	char input[17];
 	
-	printf("Por favor ingresa la cadena de numeros\n");
+	printf("Por favor ingresa la cadena cuya paridad quieres revisar: \n");
 	scanf("%s", input);
-	datos.bitsParidad = 0;
+
 	//printf("This is the output: %s\n", input);
 	memcpy(&datos.cadena, &input, 17);
 
-	//NOTA: El ejemplo del enunciado tiene un CarriageReturn al final de "Hola", por lo cual asumo que es necesario. SI NO LO ES, BORRAR EL PROXIMO FOR-LOOOP
-	//Este for loop busca el caracter nulo, lo remplaza con un Carriage Return y hace que el proximo caracter sea el caracter nulo.
-	/*for (short i = 0; i < 17; i++)
-	{
-		if (datos.cadena[i] == 0)
-		{
-			datos.cadena[i] = 0x0D;
-			if (i < 16) datos.cadena[i + 1] = 0;
-			break;
-		}
-	}*/
+	
+
+	//Creo un apuntador a datos.bitsParidad
+	unsigned short * pointer = &(datos.bitsParidad);
 
 	//Ejecuta el procedimiento para cada caracter, ignora caracteres que son invalidos
 	for (char i = 16; i >= 0; i--)
 	{
-		if (datos.cadena[i] >= 0) {
-			procedimiento(datos.cadena[i], &datos);
+		if (datos.cadena[i] > 0) {
+			procedimiento(datos.cadena[i],pointer);
 		}else {
-			procedimiento(0, &datos);
+			procedimiento(0, pointer);
 			
 		}
+		
 		//printf("%hu \n", datos.bitsParidad);
+
 	}
 
 	//Imprime el numero de resultado (que ya esta codificado bien en binario). Imprime bit por bit de mas a menos significante
 	for (char i = 16; i > 0; i--)
 	{
-		unsigned short temp = datos.bitsParidad & (int)pow(2,(i-1));
 		//Imprime un bit de la cadena de bits de paridad. Si ese bit es 0, se imprime 0. Si no, se imprime 1.
+		unsigned int temp = datos.bitsParidad & (int)pow(2,(i-1));
 		printf("%hu", temp==0? 0: 1);
 		
 	}
 	return 0;
 }
-void procedimiento(unsigned char caracter, struct Estructura * est) {
+void procedimiento(unsigned char caracter, unsigned short  * bitsActualParidad) {
 	short count = 0;
 	for (short index = 0; index < 8; index++){
+		/*
+		Este es el codigo en C correspondiente, solo que queria intentar lograrlo en Assembly
+		if ( ( (caracter>>ind) & 1) == 1) {
+			count++;
+		}*/
 		__asm {
 			mov cx, count    //move count into the cx register
 			mov al, cl       //move the least significant part of count  into AL, to save space since CX will get overwritten anyways
@@ -77,14 +79,11 @@ void procedimiento(unsigned char caracter, struct Estructura * est) {
 			mov cl, al       //move the count var into CL
 			mov count, cx   //move the count value back into the count variable in memory
 		}
-		/*
-		Este es el codigo en C correspondiente, solo que queria intentar lograrlo en Assembly
-		if ( ( (caracter>>ind) & 1) == 1) {
-			count++;
-		}*/
+		
 	}
 	count = count & 1;//gets the amount of zeroes and checks if its even or odd
-	short bitsParidad = est->bitsParidad;
+	unsigned short bitsParidad = *bitsActualParidad;
+	
 	//Aca solo sumo count a bitsDePartidad y roto a la derecha. Era mas facil con assembly, ya que con C me toca hacer dos shifts y despues una union... 
 	//Esto permite poner el bit de paridad en el digito mas significativo
 	__asm {
@@ -93,6 +92,7 @@ void procedimiento(unsigned char caracter, struct Estructura * est) {
 		ror ax, 1
 		mov bitsParidad, ax
 	}
-	est->bitsParidad = bitsParidad;
+
+	*bitsActualParidad = bitsParidad;
 
 }
